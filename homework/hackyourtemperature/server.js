82548b61654a191f36817fdeaa5b1b4f8d30e352
ayktldg <aykutuludag26@gmail.com>
2022-02-28 14:06:33 +0100

    finished project for week1

---
+++ homework/hackyourtemperature/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("hello from backend to frontend");
+});
+
+app.post("/weather", (req, res) => {
+  const cityName = req.body.cityName;
+  res.send(cityName);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Running on PORT ${PORT}`);
+});
