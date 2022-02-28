8172a381f0c9ff2a85c1180c9dfe1fa4dd8326ea
ayktldg <aykutuludag26@gmail.com>
2022-02-28 17:03:53 +0100

    remove unnecessary middleware

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -3,7 +3,6 @@ import express from "express";
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("hello from backend to frontend");
